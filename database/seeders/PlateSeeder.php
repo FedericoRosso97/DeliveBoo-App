12b19b1f92829a5ec543b55d30f0f5887da7576c
Ericson-Manalo <ericmana98@gmail.com>
2023-09-15 11:31:25 +0200

    added faker to my plate seeder

--- database/seeders/PlateSeeder.php
+++ database/seeders/PlateSeeder.php
@@ -5,13 +5,14 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Plate;
+use Faker\Generator as Faker;
 
 class PlateSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
     }
