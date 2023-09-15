2a2d5cc199b0a27b1771b3c97a6283d07e667885
FedericoRosso97 <federicorosso97@gmail.com>
2023-09-15 15:34:04 +0200

    make Restaurant controller

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Usercontroller extends Controller
+class RestaurantController extends Controller
 {
     //
 }
