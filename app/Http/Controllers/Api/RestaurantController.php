8f1e759a8de52a3e456a59a5061c4e3e21879d7a
FedericoRosso97 <federicorosso97@gmail.com>
2023-09-18 11:16:37 +0200

    make Restaurant index method

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -14,7 +14,12 @@ class RestaurantController extends Controller
     public function index()
     {
         //
+        $restaurant = Restaurant::all();
+        return response()->json([
+            'success'=>true,
+            'result'=>$restaurant
 
+        ]);
     }
 
     /**
