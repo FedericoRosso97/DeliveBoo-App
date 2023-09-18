9cb322f23a6d808d5203e510690b63040e5625f5
Ericson-Manalo <ericmana98@gmail.com>
2023-09-18 17:20:20 +0200

    migrated new columns in order's table

--- database/migrations/2023_09_18_150121_create_orders_table.php
+++ database/migrations/2023_09_18_150121_create_orders_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('status_order');
+            $table->string('phone_number');
+            $table->string('name');
+            $table->string('address');
             $table->timestamps();
         });
     }
