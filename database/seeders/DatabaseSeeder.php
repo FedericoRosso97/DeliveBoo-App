e6f09e79b3fd34660babea12c215bd03b80a1f69
Ericson-Manalo <ericmana98@gmail.com>
2023-09-20 20:38:52 +0200

    added other 2 seeder in dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            RestaurantSeeder::class,
+            TypologySeeder::class,
             OrderSeeder::class,
             PlateSeeder::class,
         ]);
