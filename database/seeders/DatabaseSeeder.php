0d67d7f71ea58b792bc56a3fd2fa0e1e1d21ae2b
Ericson-Manalo <ericmana98@gmail.com>
2023-09-20 12:36:27 +0200

    added the created orderplateseeder in databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             OrderSeeder::class,
             PlateSeeder::class,
+            OrderPlateSeeder::class,
         ]);
     }
 }
