1cd9db8eae65b350217060476861dc0d6533ca05
Ericson-Manalo <ericmana98@gmail.com>
2023-09-20 16:51:20 +0200

    created the seeder of pivot table

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Order;
+use App\Models\Plate;
+
 use Faker\Generator as Faker;
 
 
@@ -21,6 +23,8 @@ class OrderSeeder extends Seeder
                 "Delivering",
             ];
 
+            $plateIds = Plate::all()->pluck('id')->toArray();
+
         for ($i=0; $i < 100; $i++){
             $newOrder = new Order();
             $newOrder->status_order = $faker->randomElement($statusList);
@@ -28,6 +32,8 @@ class OrderSeeder extends Seeder
             $newOrder->name = $faker->name();
             $newOrder->address = $faker->address();
             $newOrder->save();
+
+            $newOrder->plates()->sync([$faker->randomElement($plateIds)]);
 
         }
 
