a77f3aa1a2aebb209d2a2fbb5d583bd2c2fe9094
FedericoRosso97 <federicorosso97@gmail.com>
2023-09-20 11:48:43 +0200

    make auth/typologyController

--- app/Http/Controllers/Auth/TypologyController.php
+++ app/Http/Controllers/Auth/TypologyController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Typology;
 
@@ -22,12 +21,6 @@ class TypologyController extends Controller
     public function create()
     {
         //
-        $typology = Typology::all();
-        return response()->json([
-            'success'=>true,
-            'result'=>$typology 
-
-        ]);
     }
 
     /**
