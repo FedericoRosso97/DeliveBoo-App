9c208315fe4cc20f07079da17236865bca27fe1c
Denise-Dichiara <dichiara.d@gmail.com>
2023-09-22 01:15:53 +0200

    Added link to index.blade.php

--- resources/views/plates/create.blade.php
+++ resources/views/plates/create.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Prova create</title>
+    <title>Create</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -17,6 +17,9 @@
     <!--header -->
     <div class="upper-header">
         <img src="/img/logo_deliveboo.png" alt="DeliveBoo brand image">
+        <a href="http://127.0.0.1:8000/plates" class="me-5">
+            Vai al menù
+        </a>
     </div>
 
     <div class="lower-header">
