930ae68b0d1f8732091f893e548beb5fdfe6a364
Ericson-Manalo <ericmana98@gmail.com>
2023-10-02 12:47:18 +0200

    fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,62 +2,48 @@
 
 @section('content')
 <div class="container d-flex justify-content-center">
-        <div class="col-md-8">
-
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <div class="my-dashboard">
-
-                        <div class="box">
-                            <div class="btn btn-one">
-                                <span>
-                                    <a href="{{route('restaurants.create')}}">
-                                        Il mio ristorante
-                                    </a>
-                                </span>
-                            </div>
-                            <div class="btn btn-one">
-                                <span>
-                                    <a href="{{ route('plates.index') }}">
-                                        Il mio menù
-                                    </a>
-                                </span>
-                            </div>
-                            <div class="btn btn-one">
-                                <span>
-                                    <a href="{{ route('orders.index') }}">
-                                        I miei ordini
-                                    </a>
-                                </span>
-                            </div>
-                            <div class="btn btn-one">
-                                <span>
-                                    <a href="http://localhost:5174/">
-                                        Home
-                                    </a>
-                                </span>
-                            </div>
-                        </div>
-
-
-
-
-                        
-
-
-
-
-
-                    </div>
-                    
+    <div class="col-md-8">
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+            @endif
 
+            <div class="my-dashboard">
+
+                <div class="box">
+                    <div class="btn btn-one">
+                        <span>
+                            <a href="{{route('restaurants.create')}}">
+                                Il mio ristorante
+                            </a>
+                        </span>
+                    </div>
+                    <div class="btn btn-one">
+                        <span>
+                            <a href="{{ route('plates.index')}}">
+                                Il mio menù
+                            </a>
+                        </span>
+                    </div>
+                    <div class="btn btn-one">
+                        <span>
+                            <a href="{{ route('orders.index') }}">
+                                I miei ordini
+                            </a>
+                        </span>
+                    </div>
+                    <div class="btn btn-one">
+                        <span>
+                            <a href="http://localhost:5174/">
+                                Home
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </div>              
+        </div>
     </div>
 </div>
 @endsection
