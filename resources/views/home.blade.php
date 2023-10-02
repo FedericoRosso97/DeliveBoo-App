52cc19c0fbbc1ccbb6eccd279ba405ba4156a7d0
Ericson-Manalo <ericmana98@gmail.com>
2023-10-02 11:37:04 +0200

    restyling buttons in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container d-flex justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header my-dashboard-header text-light">{{ __('Dashboard') }}</div>
 
-                <div class="card-body d-flex">
+
+                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <nav class="my-dashboard-nav">
-                        <ul>
-                            <li>
-                                <a href="{{route('restaurants.create')}}">
-                                    Il mio ristorante
-                                </a>
-                            </li>
+                    <div class="my-dashboard">
 
-                            <li>
-                                <a href="{{ route('plates.index') }}">
-                                    Il mio menù
-                                </a>
-                            </li>
+                        <div class="box">
+                            <div class="btn btn-one">
+                                <span>
+                                    <a href="{{route('restaurants.create')}}">
+                                        Il mio ristorante
+                                    </a>
+                                </span>
+                            </div>
+                            <div class="btn btn-one">
+                                <span>
+                                    <a href="{{ route('plates.index') }}">
+                                        Il mio menù
+                                    </a>
+                                </span>
+                            </div>
+                            <div class="btn btn-one">
+                                <span>
+                                    <a href="{{ route('orders.index') }}">
+                                        I miei ordini
+                                    </a>
+                                </span>
+                            </div>
+                            <div class="btn btn-one">
+                                <span>
+                                    <a href="http://localhost:5174/">
+                                        Home
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
 
-                            <li>
-                                <a href="{{ route('orders.index') }}">
-                                    I miei ordini
-                                </a>
-                            </li>
 
-                            <li>
-                                <a href="">
-                                    Le mie statistiche
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a href="http://localhost:5174/">
-                                    Home
-                                </a>
-                            </li>          
-                        </ul>
-                    </nav>
+
+
+                        
+
+
+
+
+
+                    </div>
                     
                 </div>
-            </div>
-        </div>
+
     </div>
 </div>
 @endsection
