b8d82c7d6ffec9093cbbfcf4aa0a86146953e07f
Denise-Dichiara <dichiara.d@gmail.com>
2023-10-02 11:32:04 +0200

    Added rstaurant's name

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -20,13 +20,21 @@ class RestaurantSeeder extends Seeder
     {
         //
         $restaurantName = [
-            "Pizzeria Ciccio",
-            
+            "Pizzeria Amalia",
+            "Pizzeria Amore",
+            "Pizzeria L'arte della Pizza",
+            "Pizzeria Bella Napoli",
+            "Pizzeria Capuano's",
         ];
 
 
         $restaurantImage = [
-            "",
+            "https://diegocortes.it/wp-content/uploads/2021/03/pizzeria-900x550.jpeg",
+            "https://m.media-amazon.com/images/I/51RPpTJkaUS._AC_UF894,1000_QL80_.jpg",
+            "https://www.traplettipittori.ch/wp-content/uploads/2019/03/pizzeria.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREQcwUQQiofxCk0-jBwT1yTSQjDvCvKQnVgMl9LCsvo4uKMpkVQ7-p0sWhSTqae16vSJc&usqp=CAU",
+            "https://www.horecabc.pl/wp-content/uploads/2022/02/pizza3.webp",
+            "https://skycolor24.pl/photo/jpg/02/20/36/51/240_F_220365180_228e9M6HbPRGIjihitrAjbYw1D05uqZn.jpg"
         ];
 
         $userIds = User::all()->pluck('id');
@@ -35,7 +43,7 @@ class RestaurantSeeder extends Seeder
         foreach ( $userIds as  $userId){
             $newRestaurant = new Restaurant();
             $newRestaurant->user_id = $userId;
-            $newRestaurant->name = $faker->randomElement($restaurantName);
+            $newRestaurant->name = $faker->$restaurantName;
             $newRestaurant->address = $faker->streetAddress();
             $newRestaurant->VAT_number = $faker->randomNumber(9,true);
             $newRestaurant->email = $faker->email();
